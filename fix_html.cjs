234006e0e8cfe65d364edf103002dee347ebcf28
Ilyas Muratbek <ilyas.muratbek.99@gmail.com>
2026-06-14 21:06:49 +0500

    Build dist

--- fix_html.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('index.html', 'utf8');
-html = html.replace(/src=\"\/images\//g, 'src=\"./images/');
-html = html.replace(/src=\"\/src\//g, 'src=\"./src/');
-fs.writeFileSync('index.html', html, 'utf8');
-console.log('Fixed paths in index.html');
--- resolve.ps1
+++
@@ -1,8 +0,0 @@
-$req = [System.Net.WebRequest]::Create('https://go.2gis.com/M2Y29')
-$req.AllowAutoRedirect = $false
-try {
-    $res = $req.GetResponse()
-    Write-Output $res.Headers['Location']
-} catch {
-    Write-Output $_.Exception.Response.Headers['Location']
-}
